3524651e5b72d23ed807c2a1a76c46c0223d7427
jiang0906 <87628036+jiang0906@users.noreply.github.com>
2021-08-29 00:50:34 +0800

    Add files via upload

--- settings.py
+++ settings.py
@@ -5,16 +5,21 @@ import pygame
 WIN_WIDTH = 1024
 WIN_HEIGHT = 600
 
+# path
+IMAGE_PATH = os.path.join(os.path.dirname(__file__), "images")
+NAME_PATH = os.path.join(os.path.dirname(__file__), "images/name")
+PART_PATH = os.path.join(os.path.dirname(__file__), "images/part")
+
 # background
-background = pygame.transform.scale(pygame.image.load(os.path.join("images/ENV.png")), (WIN_WIDTH, WIN_HEIGHT))
+background = pygame.transform.scale(pygame.image.load(os.path.join(IMAGE_PATH, "ENV.png")), (WIN_WIDTH, WIN_HEIGHT))
 
 # images name
-name_1 = pygame.transform.scale(pygame.image.load(os.path.join("images/name/1.jpg")), (500, 450))
-name_2 = pygame.transform.scale(pygame.image.load(os.path.join("images/name/2.jpg")), (500, 450))
-name_3 = pygame.transform.scale(pygame.image.load(os.path.join("images/name/3.jpg")), (500, 450))
+name_1 = pygame.transform.scale(pygame.image.load(os.path.join(NAME_PATH, "1.jpg")), (500, 450))
+name_2 = pygame.transform.scale(pygame.image.load(os.path.join(NAME_PATH, "2.jpg")), (500, 450))
+name_3 = pygame.transform.scale(pygame.image.load(os.path.join(NAME_PATH, "3.jpg")), (500, 450))
 
 # image part
-part_1 = pygame.transform.scale(pygame.image.load(os.path.join("images/part/1.jpg")), (70, 70))
-part_2 = pygame.transform.scale(pygame.image.load(os.path.join("images/part/2.jpg")), (70, 70))
-part_3 = pygame.transform.scale(pygame.image.load(os.path.join("images/part/3.jpg")), (70, 70))
+part_1 = pygame.transform.scale(pygame.image.load(os.path.join(PART_PATH, "1.jpg")), (70, 70))
+part_2 = pygame.transform.scale(pygame.image.load(os.path.join(PART_PATH, "2.jpg")), (70, 70))
+part_3 = pygame.transform.scale(pygame.image.load(os.path.join(PART_PATH, "3.jpg")), (70, 70))
 
